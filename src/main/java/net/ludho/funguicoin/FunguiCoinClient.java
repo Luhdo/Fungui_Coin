160e50d55f4e18c78087cf0ebc29ef74b4b21254
Ludho <LudhoMP@gmail.com>
2022-09-26 19:08:18 +0330

    upgraded to minecraft 1.19.2

--- src/main/java/net/ludho/funguicoin/FunguiCoinClient.java
+++ src/main/java/net/ludho/funguicoin/FunguiCoinClient.java
@@ -3,6 +3,11 @@ package net.ludho.funguicoin;
 import net.fabricmc.api.ClientModInitializer;
 
 public class FunguiCoinClient implements ClientModInitializer {
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.fabricmc.api.ClientModInitializer#onInitializeClient()
+     */
     @Override
     public void onInitializeClient() {
 
